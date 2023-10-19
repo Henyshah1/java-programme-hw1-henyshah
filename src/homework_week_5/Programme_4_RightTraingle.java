b4884a78ec5c463f54eff277f6dd9f0969e0219c
Henyshah1 <henyshah.qa@gmail.com>
2023-10-19 18:29:33 +0100

    homework

---
+++ src/homework_week_5/Programme_4_RightTraingle.java
@@ -0,0 +1,22 @@
+package homework_week_5;
+
+/**
+ * 4. Write a Java Program to print below triangle
+ *
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+public class Programme_4_RightTraingle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
